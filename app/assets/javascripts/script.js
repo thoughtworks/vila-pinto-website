479a3e94820fb5b44af805ec73edfdeb74d58d1e
Glauber Ramos <glauberamos@gmail.com>
2012-08-11 21:37:27 -0300

    fixing sobre link and indenting javascript file

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
 
-$('#menu-list').scrollspy();
+  $('#menu-list').scrollspy();
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 300) {
-    $('#menu').css('position', 'fixed').css('top', 0);
-  } else {
-    $('#menu').css('position', 'relative');
-  }
-});
-
-$(".anchor-link").click(function(event) {
-  event.preventDefault();
-  var session = $(this).attr('href');
-  var sessionPosition = $(session).offset().top;
-
-  $('body').animate({ scrollTop: sessionPosition }, 500, function() {
-      window.location.hash = session;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $('#menu').css('position', 'fixed').css('top', 0);
+    } else {
+      $('#menu').css('position', 'relative');
+    }
   });
-});
 
-$('.carousel').carousel();
+  $(".anchor-link").not(".sobre").click(function(event) {
+    event.preventDefault();
+    var session = $(this).attr('href');
+    var sessionPosition = $(session).offset().top;
 
+    $('body').animate({ scrollTop: sessionPosition }, 500, function() {
+        window.location.hash = session;
+    });
+
+    $('.sobre').click(function(e) {
+      e.preventDefault();
+    
+      $('body').animate({ scrollTop: 0 }, 500, function() {
+        window.location.hash = '';
+      });  
+    });
+  });
 });
