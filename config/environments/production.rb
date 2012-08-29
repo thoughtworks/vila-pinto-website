d94d2c029f0467e6680b07c5eeea8d37bf487d86
Jefferson Girao <jgirao@thoughtworks.com>
2012-08-29 15:52:36 -0300

    Temp doming for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,18 +64,18 @@ VilaPintoWebsite::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
-  config.action_controller.asset_host = "http://www.ceavilapinto.org.br"
-  config.action_mailer.asset_host = "http://www.ceavilapinto.org.br"
+
+  config.action_controller.asset_host = "http://vilapinto.heroku.com"
+  config.action_mailer.asset_host = "http://vilapinto.heroku.com"
   config.action_mailer.default_url_options = {
-    :host => "www.ceavilapinto.org.br"
+    :host => "http://vilapinto.heroku.com"
   }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'www.ceavilapinto.org.br',
+    :domain         => 'vilapinto.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
