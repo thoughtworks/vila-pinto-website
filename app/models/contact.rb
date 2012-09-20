34b6087feee0618150adb640d3ad10e363a0d454
ffreire <Thoughtworks@mcWork.local>
2012-09-19 21:15:44 -0300

    #4 adding a select to mail to the selected organization and mailing to the appropriate email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,21 @@
+# encoding: utf-8
 class Contact  
   include ActiveModel::Validations  
   include ActiveModel::Conversion  
   extend ActiveModel::Naming
     
-  attr_accessor :name, :email, :subject, :message  
+  attr_accessor :name, :email, :subject, :message, :to  
    
-  validates_presence_of :name, :email, :subject, :message 
+  validates_presence_of :name, :email, :subject, :message, :to
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i  
   validates_length_of :content, :maximum => 500  
+  
+  INSTITUTIONS = {
+    'CEA' => 'cea',
+    'CTVP' => 'ctvp',
+    'Cejak' => 'cejak',
+    'Vovó Belinha' => 'vovo'
+  }
 
   def initialize(attributes = {})  
     attributes.each do |name, value|  
