25ab9cbb6e12adbfcc40a5039e30ab4fd294886f
Jefferson Girao <jgirao@thoughtworks.com>
2012-09-26 19:36:30 -0300

    Removing blank trailing white spaces

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,15 @@
 # encoding: utf-8
-class Contact  
-  include ActiveModel::Validations  
-  include ActiveModel::Conversion  
+class Contact
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
   extend ActiveModel::Naming
-    
-  attr_accessor :name, :email, :subject, :message, :to  
-   
+
+  attr_accessor :name, :email, :subject, :message, :to
+
   validates_presence_of :name, :email, :subject, :message, :to
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i  
-  validates_length_of :content, :maximum => 500  
-  
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_length_of :content, :maximum => 500
+
   INSTITUTIONS = {
     'CEA' => 'cea',
     'CTVP' => 'ctvp',
@@ -17,14 +17,14 @@ class Contact
     'Vovó Belinha' => 'vovo'
   }
 
-  def initialize(attributes = {})  
-    attributes.each do |name, value|  
-      send("#{name}=", value)  
-    end  
-  end  
-    
-  def persisted?  
-    false  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
   end
 
   def content
