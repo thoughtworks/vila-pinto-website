4898a893845049d7d5d5ab8a38dc3c6fe3ee39ef
Glauber Ramos <glauberamos@gmail.com>
2012-09-28 00:26:42 -0300

    change the compress to false in production to test if respond.js will work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ VilaPintoWebsite::Application.configure do
   config.assets.precompile += %w( respond.min.js )
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
