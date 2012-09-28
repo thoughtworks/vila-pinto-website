1705f0e748fe311582f84852bbf31b5e5b4fe1c6
Glauber Ramos <glauberamos@gmail.com>
2012-09-28 00:00:07 -0300

    trying to make respond.js work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,11 +11,15 @@ VilaPintoWebsite::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  # Precompile js
+  config.assets.precompile += %w( respond.js )
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+
 
   # Generate digests for assets URLs
   config.assets.digest = true
