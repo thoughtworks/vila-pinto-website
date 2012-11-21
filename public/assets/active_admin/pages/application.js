ee4eb4e112dea9cc7b22a8524e85537e61acbcec
Glauber Ramos <glauberamos@gmail.com>
2012-11-21 20:52:13 -0200

    removing asset_host frmo config

---
+++ public/assets/active_admin/pages/application.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return $(".datepicker").datepicker({dateFormat:"yy-mm-dd"}),$(".clear_filters_btn").click(function(){return window.location.search="",!1}),$(".dropdown_button").popover()})}).call(this);
