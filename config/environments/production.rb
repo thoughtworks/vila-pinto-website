e13929b677935e5cc7b28f430db054a80c7c09c8
Glauber Ramos <glauberamos@gmail.com>
2012-11-21 20:59:23 -0200

    commenting asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ VilaPintoWebsite::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_controller.asset_host = "http://www.ceavilapinto.org.br"
+  #config.action_controller.asset_host = "http://www.ceavilapinto.org.br"
   config.action_mailer.asset_host = "http://www.ceavilapinto.org.br"
   config.action_mailer.default_url_options = {
     :host => "http://www.ceavilapinto.org.br"
