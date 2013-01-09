ccceb92cfc033ca624ffb6d3f4c8fac2d71d503e
ronualdo <raxmac@gmail.com>
2013-01-09 14:50:29 -0300

    add generate sitemap task to the deploy

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = ENV['HOST']
+SitemapGenerator::Sitemap.default_host = args[:host] || ENV['HOST']
 SitemapGenerator::Sitemap.create do
   YAML.load_file('config/sitemap.yml').map do |url|
     add url['loc'], priority: url['priority']
