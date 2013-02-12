f3dd189324cd7a1617a4ace68b7221faf86f867e
Fabio Maia <fabioromm@gmail.com>
2013-02-12 10:59:06 -0300

    85 - Create project model

---
+++ app/models/project.rb
@@ -0,0 +1,13 @@
+class Project < ActiveRecord::Base
+  attr_accessible :category, :description, :name, :visible
+
+  values_for :category, :has => [:children_and_teenagers, :community], :add => [:predicate_methods, :constants]
+
+  validates :name, :presence => true, :length => { :maximum => 200 }
+  validates :visible, :presence => true
+  validates :description, :presence => true, :length => { :maximum => 500 }
+  validates :category, :presence => true
+
+  scope :children_and_teenagers, where(:category => Project::CATEGORY_CHILDREN_AND_TEENAGERS)
+  scope :community, where(:category => Project::CATEGORY_COMMUNITY)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109221628) do
+ActiveRecord::Schema.define(:version => 20130212132633) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -178,6 +178,15 @@ ActiveRecord::Schema.define(:version => 20130109221628) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "type"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.string   "category"
+    t.boolean  "visible"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
