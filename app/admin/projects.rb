26446fc8b5f8fc3cd44ace3135303c9fe7e13ef1
Jefferson <jsantos@thoughtworks.com>
2013-04-02 23:58:46 -0300

    #86: Adding complex validation for Frequency indicator (Jefferson)

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -3,8 +3,6 @@ ActiveAdmin.register Project do
   config.clear_sidebar_sections! # remove filter sidebar
   config.paginate = false
 
-  puts form
-
   scope :children_and_teenagers, :default => true
   scope :community
 
@@ -21,43 +19,5 @@ ActiveAdmin.register Project do
   end
 
   form :partial=> "indicators_form"
-=begin
-  form do |f|
-    f.inputs do
-      f.input :name
-      f.input :category, :as => :select, :collection => project_category_options
-      f.input :visible
-      f.input :description
-    end
 
-    f.inputs do
-      f.input :attendance_value, :input_html => { :class => 'short_value'  }, :wrapper_html => { :class => 'project_indicator_label_value_column' }
-      f.input :show_attendance, :label=>'',  :wrapper_html => { :class => 'indicator_show_column', :style => 'display:block;float:right; width:10%;  '   }
-      f.input :attendance_day_monday 
-      f.input :attendance_day_tuesday
-      f.input :attendance_day_wednesday
-      f.input :attendance_day_thursday
-      f.input :attendance_day_friday
-      f.input :attendance_day_saturday
-      f.input :attendance_day_sunday
-      f.input :show_attendance_days
-      f.input :frequency_value
-      f.input :frequency_unit, :as => :select, :collection => project_frequency_unit_options
-      f.input :show_frequency
-      f.input :attendance_shift_morning
-      f.input :attendance_shift_afternoon
-      f.input :attendance_shift_night
-      f.input :show_attendance_shift
-      f.input :attendance_goal_value
-      f.input :show_attendance_goal
-      f.input :meals_quantity_value
-      f.input :meals_quantity_unit, :as => :select, :collection => project_meals_options
-      f.input :show_meals_quantity
-      f.input :available_spots
-      f.input :show_available_spots
-    end
-
-    f.actions
-  end
-=end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,31 +1,53 @@
 class Project < ActiveRecord::Base
+
+  
+
   attr_accessible :category, :description, :name, :visible
   attr_accessible :attendance_value, :show_attendance
   attr_accessible :attendance_day_sunday, :attendance_day_monday,:attendance_day_tuesday,:attendance_day_wednesday,:attendance_day_thursday
   attr_accessible :attendance_day_friday, :attendance_day_saturday
   attr_accessible :show_attendance_days
   attr_accessible :frequency_value, :frequency_unit, :show_frequency
-  attr_accessible :attendance_shift_morning, :attendance_shift_afternoon, :attendance_shift_night 
-  attr_accessible :show_attendance_shift
+  attr_accessible :attendance_shift_morning, :attendance_shift_afternoon, :attendance_shift_night , :show_attendance_shift
   attr_accessible :attendance_goal_value, :show_attendance_goal
   attr_accessible :meals_quantity_value, :meals_quantity_unit, :show_meals_quantity
   attr_accessible :available_spots, :show_available_spots
 
   values_for :category, :has => [:children_and_teenagers, :community], :add => [:predicate_methods, :constants]
-  #values_for :attendance_days, :has => [ :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday], :add => [:predicate_methods, :constants]
-  values_for :frequency_unit, :has => [:week, :month], :add => [:predicate_methods, :constants]
-  #values_for :attendance_shift, :has => [:morning, :afternoon, :night], :add => [:predicate_methods, :constants]
-  values_for :meals_quantity_unit, :has => [:day, :month], :add => [:predicate_methods, :constants]
+  values_for :frequency_unit, :has => [:week, :month], :add => [:predicate_methods, :constants], :allow_nil => true
+  values_for :meals_quantity_unit, :has => [:day, :month], :add => [:predicate_methods, :constants], :allow_nil => true
 
   validates :name, :presence => true, :length => { :maximum => 200 }
   validates :description, :presence => true, :length => { :maximum => 500 }
-  validates :attendance_value, :numericality => { :greater_than => 0, :less_than => 100000 }
-  validates :frequency_value, :numericality => { :greater_than => 0, :less_than => 100 }
-  validates :attendance_goal_value, :numericality => { :greater_than => 0, :less_than => 100000 }
-  validates :meals_quantity_value, :numericality => { :greater_than => 0, :less_than => 100000 }
-  validates :available_spots, :numericality => { :greater_than => 0, :less_than => 10000 }
+  #indicators
+  validates :attendance_value, :numericality => { :greater_than => 0, :less_than => 100000 }, :allow_nil => true
+  validates :frequency_value, :numericality => { :greater_than => 0, :less_than => 100 }, :allow_nil => true
+  validates :attendance_goal_value, :numericality => { :greater_than => 0, :less_than => 100000 }, :allow_nil => true
+  validates :meals_quantity_value, :numericality => { :greater_than => 0, :less_than => 100000 }, :allow_nil => true
+  validates :available_spots, :numericality => { :greater_than => 0, :less_than => 10000 }, :allow_nil => true
+
+  validate :invalid_frequency
+
+  #validates :frequency_unit, :allow_nil => true
+  #validates :meals_quantity_unit, :allow_nil => true
 
   scope :children_and_teenagers, where(:category => Project::CATEGORY_CHILDREN_AND_TEENAGERS)
   scope :community, where(:category => Project::CATEGORY_COMMUNITY)
 
+  def error_message(message)
+    I18n.t("activerecord.attributes.project.errors.#{message}")
+  end  
+  
+
+  def invalid_frequency
+    
+    errors.add(:frequency_unit, error_message(:frequency_without_unit) )  if not frequency_value.nil? and  frequency_unit.nil? 
+    errors.add(:frequency_value, error_message(:frequency_without_value) )  if not frequency_unit.nil? and frequency_value.nil? 
+    errors.add(:show_frequency, error_message(:show_without_frequency) )  if show_frequency and frequency_value.nil? 
+    
+  end
+
+
 end
+
+
