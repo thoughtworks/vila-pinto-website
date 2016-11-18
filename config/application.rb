ee513377e0541a28672725687028b4c24fbd2907
Glauber Ramos <gramos@thoughtworks.com>
2016-11-18 18:23:11 -0300

    adding config for I18n

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module VilaPintoWebsite
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    I18n.config.enforce_available_locales = false
     config.i18n.default_locale = 'pt-BR'
 
     # Configure the default encoding used in templates for Ruby 1.9.
@@ -60,7 +61,7 @@ module VilaPintoWebsite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
     config.assets.initialize_on_precompile = false
   end
 end
